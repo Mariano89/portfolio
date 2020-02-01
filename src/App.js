06dbcfd41f5a5dd1d41694e4225ba0431db19922
Mariano Echegoyen <mariano.echegoyen@dng.com>
2020-01-31 16:03:39 -0800

    Update logo and link

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import logo from './images/logo.svg';
+import face from './images/face.png';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={face} className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="mailto:marianoe1989@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          email
         </a>
       </header>
     </div>
