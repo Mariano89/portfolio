9fb09afff67489f1e816bf2c896b005cba578240
Mariano89 <marianoe1989@gmail.com>
2020-05-07 23:23:51 -0700

    Use CSS animation over GSAP

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
 import face from '../images/face.png';
-import { TimelineLite, CSSPlugin } from "gsap/all";
 
 class Home extends Component {
-
-  constructor(props) {
-    super(props);
-    this.myFace = null;
-    this.myTween = new TimelineLite({paused:false});
-  }
-
-  componentDidMount() {
-    this.myTween.to(this.myFace, 1, {
-      transform: "scale(1)",
-      ease: "Power3.easeInOut"
-    });
-  }
-
   render() {
     return (
       <main id="home">
-          <div className="logo-container" ref={div => this.myFace = div}>
+          <div className="logo-container">
             <img 
               src={face} 
               className="logo" 
