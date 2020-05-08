df63743084b00d430c7001088d9576768de21c38
Mariano Echegoyen <mariano.echegoyen@dng.com>
2020-05-07 19:49:35 -0700

    Scale face

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import face from './images/face.png';
+import React, { Component } from 'react';
+import Home from './pages/Home';
 import './styles/css/main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={face} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="mailto:marianoe1989@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          email
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  } 
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import face from '../images/face.png';
+import { TimelineLite, CSSPlugin } from "gsap/all";
+
+class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.myFace = null;
+    this.myTween = new TimelineLite({paused:false});
+  }
+
+  componentDidMount() {
+    this.myTween.to(this.myFace, 1, {
+      transform: "scale(1)",
+      ease: "Power3.easeInOut"
+    });
+  }
+
+  render() {
+    return (
+      <main id="home">
+        <div>
+          <div className="logo-container" ref={div => this.myFace = div}>
+            <img 
+              src={face} 
+              className="logo" 
+              alt="logo" 
+              
+            />
+          </div>
+          <div className="link-container">
+            <a
+              className="link"
+              href="mailto:marianoe1989@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              email
+            </a>
+          </div>
+        </div>
+      </main>
+    )
+  }
+}
+
+export default Home;
