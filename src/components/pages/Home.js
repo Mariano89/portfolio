dfd1679b9477371c296d3611a6b11f92e90b0684
Mariano89 <marianoe1989@gmail.com>
2021-04-22 13:58:25 -0700

    Add buy me a beer button

---
+++ src/components/pages/Home.js
@@ -0,0 +1,105 @@
+import React, { useRef, useEffect } from 'react';
+import { TweenMax, TimelineLite } from 'gsap';
+import face from '../../images/face.png';
+import beer from '../../images/modelo.png';
+
+const Home = () => {
+  // let script = document.createElement('script')
+  // script.setAttribute('data-name', 'bmc-button')
+  // script.src = 'https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js'
+  // script.setAttribute('data-slug', 'marscode')
+  // script.setAttribute('data-color', '#FFDD00')
+  // script.setAttribute('data-emoji', '🍺')
+  // script.setAttribute('data-font', 'Inter')
+  // script.setAttribute('data-text', 'Buy me a beer')
+  // script.setAttribute('data-outline-color', '#000000')
+  // script.setAttribute('data-font-color', '#000000')
+  // script.setAttribute('data-coffee-color', '#ffffff')
+  // script.async = true
+  // script.onload=function(){
+  //   var evt = document.createEvent('Event');  
+  //   evt.initEvent('DOMContentLoaded', false, false);  
+  //   window.dispatchEvent(evt);
+  // }
+
+
+  const faceRef = useRef();
+  const scaleRef = useRef();
+  const beerRef = useRef();
+
+  const animateFaceToNav = () => {
+    document.querySelector('.logo-container').classList.add('shrink-face')
+    document.querySelector('.logo').classList.add('stop-spin')
+    // document.querySelector('.center').classList.remove('center')
+  }
+
+  const toggleSpin = () => {
+    document.querySelector('.logo').classList.contains('stop-spin') ? 
+      document.querySelector('.logo').classList.remove('stop-spin') :
+      document.querySelector('.logo').classList.add('stop-spin')
+  }
+
+  useEffect(()=> {
+    const donateButton = document.querySelector('.bmc-btn-container');
+    document.querySelector('.donate-button-container').appendChild(donateButton)
+
+    TweenMax.fromTo(scaleRef.current, 1, {
+      transform: "scale(0.1)"
+    }, {
+      transform: "scale(1)",
+      rotation: "1080"
+    })
+    TweenMax.to(beerRef.current, 1, {
+      delay: 1.8,
+      opacity: 1,
+      top: "-145px"
+    })
+    TweenMax.to(donateButton, 1, {
+      delay: 4.5,
+      opacity: 1
+    })
+    const tl = new TimelineLite({repeat: 10, delay: 3})
+    tl
+      .to(beerRef.current, .035, { transform: "rotate(-15deg)" })
+      .to(beerRef.current, .035, { transform: "rotate(15deg)" })
+      .to(beerRef.current, .035, { transform: "rotate(0deg)" })
+  }, [])
+
+
+  return (
+    <main id="home">
+      <div className="logo-container">
+        <div className="" ref={scaleRef}>
+          <img 
+            className="logo" 
+            ref={faceRef}
+            src={face} 
+            alt="logo" 
+            // onClick={toggleSpin}
+          />
+          <img
+            className="beer"
+            ref={beerRef}
+            src={beer}
+          />
+          <div className="donate-button-container"></div>
+          {/*<span id="text"></span>*/}
+        </div>
+      </div>
+      {/*<div className="link-container">
+        <p className="link" onClick={animateFaceToNav}>play</p>
+        <span> | </span>
+        <a
+          className="link"
+          href="mailto:marianoe1989@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          email
+        </a>
+      </div>*/}
+    </main>
+  )
+}
+
+export default Home;
